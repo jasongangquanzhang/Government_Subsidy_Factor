c78ac4a2aa5cea1ea293bb96fb5af0cfc2bae2c5
Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2023-07-04 17:24:07 +0800

    add commit

--- govt_func.py
+++ govt_func.py
@@ -23,7 +23,7 @@ def get_quarter(df: pd.DataFrame):
     df_q2 = df_q2.sort_index(axis=1) # sort the date
     df_q2 = df_q2.interpolate(axis=1)  # if Q1 is nan, it would become q2/2
     df_q2 = df_q2.diff(1, axis=1)  # difference
-    df_q2 = df_q2.filter(like="Q2", axis=1).replace(0, np.nan)
+    df_q2 = df_q2.filter(like="Q2", axis=1).replace(0, np.nan) # replace 0 with nan
 
     # Q3
     df_q3 = df.loc[
